a3176e0ac4f157296f92d7dfd21723a92a1654f5
KateKompaniec <kompanieckatya24@gmail.com>
2022-10-15 10:44:10 +0300

    added folders

---
+++ client/src/components/Content/Content.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ListHero from '../ListHero/ListHero'
+
+export default function Content() {
+  return (
+    <div>
+      <ListHero/>
+    </div>
+  )
+}
---
+++ client/src/components/ListHero/ListHero.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hero from '../Hero/Hero'
+
+export default function ListHero() {
+  return (
+    <div>
+      <Hero/>
+    </div>
+  )
+}
